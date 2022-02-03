423f8493cb02a0962df048576e52238d9686a6c2
shiva-balre-sofka <shiva.balre@sofka.com.co>
2022-02-03 13:27:50 +0530

    getByAltText Function

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import {render, screen} from '@testing-library/react'
+import App from './App'
+//
+it('should show an image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const img =screen.getByAltText('logo');
+  expect(img).toBeInTheDocument()
+
+})
