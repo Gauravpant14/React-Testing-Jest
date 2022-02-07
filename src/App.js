d159902d36a8262c4e575d6f2298c0f5edd6126a
shiva-balre-sofka <shiva.balre@sofka.com.co>
2022-02-07 18:44:29 +0530

    basic uses explaned

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <label>
+
+        Name
+        <input type="text" />
+      </label>
+      <label htmlFor="age">Age</label>
+      <input type="number" id="age"/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p title="tooltip">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -14,11 +21,15 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          data-testid="link_any_thing"
         >
           Learn React
         </a>
         <button aria-pressed="true"></button>
       </header>
+      <input type="text" value="Gaurav" onChange={(e) => console.log(e)}/>
+      <input type="text" defaultValue="anything" onChange={(e) => console.log(e)} placeholder="Enter name"/>
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,22 @@
 import {render, screen} from '@testing-library/react'
 import App from './App'
+
+beforeEach(() => {
+  //before each test will render app everytime
+  render(<App/>);
+})
+
 //
+
 it('should show an image', () => {
-  render(<App />);
+  // render(<App />);
   const img =screen.getByAltText('logo');
   expect(img).toBeInTheDocument()
 
 })
 
 it('should show an element with a role attribute',() => {
-  render(<App/>);
+  // render(<App/>);
   //We are not assigning element to varibale instead of that directly using inside expect function
   //trying to get a particular element using it's role attribute (role can be img ,heading etc...)
   expect(screen.getByRole('link', { name: 'Learn React'})).toBeInTheDocument();
@@ -18,3 +25,43 @@ it('should show an element with a role attribute',() => {
 
 })
 
+it('should show text in document', () => {
+  // render(<App/>);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+
+})
+
+it('should have a title', () => {
+  // render(<App/>);
+  expect(screen.getByTitle('tooltip')).toBeInTheDocument(); //search for particular element that has a title attribute
+})
+
+it('testing component using testById', () => {
+  // render(<App/>);
+  //getByTestId looks for component with attribute data-testid
+  expect(screen.getByTestId('link_any_thing')).toBeInTheDocument(); //When component is deeply nested you can use this for any component
+})
+
+
+//looks for form element with particular value 
+it('should have a value', () => {
+  expect(screen.getByDisplayValue('Gaurav')).toBeInTheDocument();
+})
+it('should have a value', () => {
+  expect(screen.getByDisplayValue('anything')).toBeInTheDocument();
+})
+
+//it looks for an input element with particular placeholder 
+it('should have a placeholder text', () => {
+  expect(screen.getByPlaceholderText('Enter name')).toBeInTheDocument();
+})
+
+
+//it looks for particular label with text provided to it and relate that label to form input
+
+it('should have a label text',()=>{
+  expect(screen.getByLabelText('Name')).toBeInTheDocument();
+})
+it('should have a label text',()=>{
+  expect(screen.getByLabelText('Age')).toBeInTheDocument();
+})
